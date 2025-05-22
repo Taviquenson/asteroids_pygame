7750e49b3c2d724c60eaf559fe263f46c4a5502d
Taviquenson <tavo2070@gmail.com>
2025-05-22 14:43:22 -0700

    add asteroid field

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import pygame
 
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,8 +15,24 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 # delta time: used to represent the amount of time that has passed since the last frame was drawn
 
+    # declare groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Set the static "container" field of the sprite classes
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    # IMPORTANT: Instances of sprites like "player" MUST be created after their clases are added to the groups/containers like in previous line
+    # NOTICE that even though object "player" gets marked as an unused
+    # variable, it is being used trough the call done on the containers 
     # instantiate a Player
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
+    
+
 
 
     while True:
@@ -26,11 +44,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
+        # no need to iterate "updatable" container because the pygame library knows how to handle this efficiently
+        updatable.update(dt)
+
+        # whereas with a "draw" function, you need to tell Pygame where to draw each sprite
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip() # display updates
 
